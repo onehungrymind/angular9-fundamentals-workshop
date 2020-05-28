de57aa07421a464a16447b5ce466599e988e6a57
Lukas Ruebbelke <simpul@gmail.com>
2020-05-28 13:11:26 -0700

    update: template forms example

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -32,13 +32,34 @@ export class CoursesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.resetSelectedCourse();
+  }
+
+  resetSelectedCourse() {
+    const emptyCourse = {
+      id: null,
+      title: '',
+      description: '',
+      percentComplete: 0,
+      favorite: false
+    };
+
+    this.selectedCourse = emptyCourse;
   }
 
   selectCourse(course) {
     this.selectedCourse = course;
   }
 
+  saveCourse() {
+    console.log('SAVE SOURCE!');
+  }
+
   deleteCourse(courseId) {
     console.log('COURSE DELETED!', courseId);
+  }
+
+  cancel() {
+    this.resetSelectedCourse();
   }
 }
