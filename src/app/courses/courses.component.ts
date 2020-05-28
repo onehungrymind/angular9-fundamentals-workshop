c3b272073e562443c2651d7d397d6086e498e1ff
Lukas Ruebbelke <simpul@gmail.com>
2020-05-28 11:50:31 -0700

    update: challenge description

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -6,12 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+  // CHALLENGE
+  // STEP 01: Display courses using ngFor
+  // STEP 02: Add event handler to select course
+  // STEP 03: Display raw json of selected course
+
   courses = [
     {
       id: 1,
       title: 'Angular 9 Fundamentals',
       description: 'Learn the fundamentals of Angular 9',
       percentComplete: 26,
+      favorite: true
+    },
+    {
+      id: 2,
+      title: 'JavaScript The Really REALLY HARD PARTS',
+      description: 'Worship Will Sentance',
+      percentComplete: 50,
       favorite: true
     }
   ];
