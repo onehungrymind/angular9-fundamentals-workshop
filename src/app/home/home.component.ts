0fb394ea412fcbaa0c5a998ec078c3f3f31674b6
Lukas Ruebbelke <simpul@gmail.com>
2020-05-28 14:04:58 -0700

    update: updating challenge

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,13 @@ export class HomeComponent implements OnInit {
   title = 'Hello Workshop';
   currentLesson = null;
 
+  // CHALLENGE
+  // STEP 01: Create a LessonsService
+  // hint: ng g shared/services/lessons -d
+  // STEP 02: Add the lessons service to app.module
+  // STEP 03: Inject lessons service into component
+  // STEP 04: Move lessons to service and consume in component
+
   courseLessons = [
     { title: 'Hello Angular' },
     { title: 'Component Fundamentals' },
