2a0b2b2b2bcaf28581caa176075c45c9199232d8
Josh de Gouveia <josh.degouveia@hach.com>
2020-05-29 00:12:17 -0700

    fix tests

--- src/app/shared/services/courses.service.spec.ts
+++ src/app/shared/services/courses.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { CoursesService } from './courses.service';
@@ -6,7 +7,14 @@ describe('CoursesService', () => {
   let service: CoursesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: jasmine.createSpyObj('HttpClient', ['get', 'post', 'put', 'delete'])
+        }
+      ]
+    });
     service = TestBed.inject(CoursesService);
   });
 
