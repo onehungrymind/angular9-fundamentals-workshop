b07d697244e9ebb539068637636ea0d483d96979
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-11-30 16:37:30 -0600

    learning if/else

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   title = 'Hello World';
-  themeColor = 'red';
+  currentLesson = null;
   courseLessons = [
     { title: 'Hello Angular' },
     { title: 'Component Fundamentals' },
@@ -24,4 +24,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  selectLesson(lesson) {
+    this.currentLesson = lesson;
+  }
 }
