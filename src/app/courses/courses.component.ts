0d47d68aa1775f417e9f0c02820fc4bc24732d04
George Shalamberidze <giorgishalamberidze1995@gmail.com>
2021-09-08 23:02:06 +0400

    Forms

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+  currentCourse = null
   courses = [
     {
       id: 1,
@@ -13,12 +14,48 @@ export class CoursesComponent implements OnInit {
       description: 'Learn the fundamentals of Angular 9',
       percentComplete: 26,
       favorite: true
+    },
+    {
+      id: 2,
+      title: 'Some Other Stuff',
+      description: 'Im gonna be successful oneday',
+      percentComplete: 85,
+      favorite: false
     }
   ];
 
   constructor() { }
 
+  selectCourse(course) {
+    this.currentCourse = course
+  }
+
+  deleteCourse(id) {
+    console.log(id)
+  }
+
+  resetSelectedCourse() {
+    const emptyCourse = {
+      id: null,
+      title: '',
+      description: "",
+      percentComplete: 0,
+      favorite: false
+    }
+
+    this.currentCourse = emptyCourse
+  }
+
   ngOnInit(): void {
+    this.resetSelectedCourse()
+  }
+
+  cancel() {
+    this.resetSelectedCourse()
+  }
+
+  saveCourse() {
+    console.log("Saev Course")
   }
 
 }
